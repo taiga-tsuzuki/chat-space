039469da94d40184b34d4d24920faf131c12e114
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-02-18 12:28:08 +0900

    add_automaticupdating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,8 @@
 $(function(){ 
+  
+  // last_message_id = $('.message:last').data("message-id");
+  // console.log(last_message_id);
+
   function buildHTML(message){
     if ( message.image ) {
       var html =
@@ -39,29 +43,124 @@ $(function(){
     return html;
     };
   }
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.main__comments__comment').append(html); 
-    $('form')[0].reset();
-    $('.main__comments').animate({ scrollTop: $('.main__comments')[0].scrollHeight});
-  })
-  .fail(function(){
-    alert("メッセージ送信に失敗しました")
-  })
-  .always(function() {
-    $('.main__form__new_message__send').prop('disabled', false);
-  });  
-})
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main__comments__comment').append(html); 
+      $('form')[0].reset();
+      $('.main__comments').animate({ scrollTop: $('.main__comments')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました")
+    })
+    .always(function() {
+      $('.main__form__new_message__send').prop('disabled', false);
+    });
+  });
+
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.main__comments__comment').append(insertHTML);
+        $('.main__comments').animate({ scrollTop: $('.main__comments')[0].scrollHeight});
+        // $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
+
+
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+          <img src="${message.image}" class="lower-message__image" >
+        </div>
+      </div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <img src="${message.image}" class="lower-message__image" >
+        </div>
+      </div>`
+    };
+    return html;
+  };
+
+
+  //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
 });
